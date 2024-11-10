c0d77c7a793b9b64e95cb937611f83287c577da9
sallys03 <sallysferreira03@gmail.com>
2024-11-09 21:28:29 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,29 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso
+let vitorias = 105;
+let derrotas = 5;
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
+
